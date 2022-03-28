1530b81a6243e5a6a45e993964b1b9e0391a8d0b
scrappy2k20 <zrdrhoades@gmail.com>
2022-03-28 18:22:40 -0400

    created LeadTask.java

---
+++ src/test/java/LeadTask.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class LeadTask {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[10];
+        int size = numbers.length;
+
+        for(int i = 0; i < size; i++) {
+            int random = (int)(Math.random() * 100);
+            numbers[i] = random;
+        }
+
+        String before = Arrays.toString(numbers);
+        System.out.println("Before: " + before);
+
+        Arrays.toString(numbers);
+
+        String after = Arrays.toString(numbers);
+        System.out.println("After: " + after);
+    }
+}
